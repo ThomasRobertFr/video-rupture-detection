2b243cebe6e4596d8260ed9b6042d8c2a4ab0536
Thomas <trobert94@gmail.com>
2014-12-17 17:58:16 +0100

    Fix init of ruptStart in case of rupture at the beggining

--- detectionRupture.m
+++ detectionRupture.m
@@ -3,7 +3,7 @@ function [zones, points] = detectionRupture(D, C)
 zones = D > C;
 points = [];
 
-ruptStart = 0;
+ruptStart = 1;
 for i = 2:length(zones)
     if zones(i) == 1 && zones(i-1) == 0
         ruptStart = i;
